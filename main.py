435f5c7b81fad4013a89d2101c56119cf92f1d81
Jivesh-Bisht <110481740+Jivesh-Bisht@users.noreply.github.com>
2024-05-31 23:16:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ class MyClient(discord.Client):
 
     async def on_message(self,message):
     # Check if the message is from the specified server and user
-        if (message.guild and message.guild.id in [1236380949663711313]) and (message.author.id == 1150448986264698980):
+        if (message.guild and message.guild.id in [1236380949663711313,1246083692376625216]) and (message.author.id == 1150448986264698980):
             print(f'Message received user {message.author.name}: {message.content}')
             for i in range(1):
                 print(type(message.components[0].children[i]))
