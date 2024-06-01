44ed3d187fa25bf43a3b6e4fffc9b1c9976eace5
Jivesh-Bisht <110481740+Jivesh-Bisht@users.noreply.github.com>
2024-06-01 13:00:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,12 +22,11 @@ class MyClient(discord.Client):
     # Check if the message is from the specified server and user
         if (message.guild and message.guild.id in [1236380949663711313,1246083692376625216]) and (message.author.id == 1150448986264698980):
             print(f'Message received user {message.author.name}: {message.content}')
-            if message.components != []:
-                for i in range(1):
-                    print(type(message.components[0].children[i]))
-                    if message.components[0].children[i].label == "Enter":
-                        time.sleep(random.randint(5,10))
-                        await message.components[0].children[i].click()
+            for i in range(1):
+                print(type(message.components[0].children[i]))
+                if message.components[0].children[i].label == "Enter":
+                    time.sleep(random.randint(5,10))
+                    await message.components[0].children[i].click()
             #await message.components[0].children[0].click()
 
 app.run(host='0.0.0.0', port=port)
