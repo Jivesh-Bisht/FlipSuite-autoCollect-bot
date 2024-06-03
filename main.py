4d0a4b0d526c2fe68de63d8aa161a51b5fe72212
Jivesh-Bisht <110481740+Jivesh-Bisht@users.noreply.github.com>
2024-06-03 22:20:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,16 +19,14 @@ class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as', self.user)
 
-    async def on_message(self,message):
-    # Check if the message is from the specified server and user
-        if (message.guild and message.guild.id in [1236380949663711313,1246083692376625216]) and (message.author.id == 1150448986264698980):
+   async def on_message(self, message):
+        if (message.author.id == 1150448986264698980):
             print(f'Message received user {message.author.name}: {message.content}')
-            for i in range(1):
-                print(type(message.components[0].children[i]))
-                if message.components[0].children[i].label == "Enter":
-                    time.sleep(random.randint(5,10))
-                    await message.components[0].children[i].click()
-            #await message.components[0].children[0].click()
+            for component in message.components:
+                for child in component.children:
+                    if child.label == "Enter":
+                        await asyncio.sleep(random.randint(2, 5))
+                        await child.click()
 def run_server():
     app.run(host='0.0.0.0', port=port)
 def run_bot():
